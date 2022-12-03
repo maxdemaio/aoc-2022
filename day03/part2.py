0bf827f532635ac9d8f4ae0bf670842858180567
maxdemaio <maxdman321@gmail.com>
2022-12-03 10:24:21 -0500

    solve day 3

--- day03/part2.py
+++ day03/part2.py
@@ -4,23 +4,22 @@ import pytest
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def set_intersection(set1: set, set2: set) -> list[str]:
-    return [x for x in set1 if x in set2]
+def set_intersection(set1: set, set2: set, set3: set) -> list[str]:
+    return [x for x in set1 if (x in set2 and x in set3)]
 
 
 def compute(s: str) -> int:
     lines = s.splitlines()
-    pointTotal = 0
+    point_total = 0
+    chunk_size = 3
 
-    for line in lines:
-        ruck1 = line[:len(line)//2]
-        ruck2 = line[len(line)//2:]
-
-        # convert to sets
-        ruck1set = set(ruck1)
-        ruck2set = set(ruck2)
-
-        intersection: list(str) = set_intersection(ruck1set, ruck2set)
+    for i in range(0, len(lines), chunk_size):
+        # grab 3 rucksacks at a time
+        three_rucksacks = lines[i:i+chunk_size]
+        set1 = set(three_rucksacks[0])
+        set2 = set(three_rucksacks[1])
+        set3 = set(three_rucksacks[2])
+        intersection: list(str) = set_intersection(set1, set2, set3)
 
         # if there is an intersection, do the stuff
         if len(intersection) == 1:
@@ -28,13 +27,15 @@ def compute(s: str) -> int:
             print(intersection)
             print(ord(letter))
             if letter.islower() and letter.isalpha():
-                pointTotal += ord(letter) - 96
+                point_total += ord(letter) - 96
             elif letter.isupper() and letter.isalpha():
-                pointTotal += ord(letter) - 38
+                point_total += ord(letter) - 38
         else:
             raise Exception("no intersection in rucksacks!")
             return 1
-    return pointTotal
+
+        print(three_rucksacks)
+    return point_total
 
 # TODO: change for the small example given
 INPUT_S = '''\
