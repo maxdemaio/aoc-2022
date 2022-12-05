711bc35fb19586fcb590d3bfd329eac78552d9bd
maxdemaio <maxdman321@gmail.com>
2022-12-05 12:40:58 -0500

    finish day 5 parts 1 and 2

--- day05/part1.py
+++ day05/part1.py
@@ -1,89 +1,45 @@
 import os.path
 import pytest
-from collections import deque
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> str:
-    stack_1 = deque()
-    stack_2 = deque()
-    stack_3 = deque()
-    directions = deque()
+    # num of columns (change to 3 for example, 9 for my input)
+    num_of_stacks = 9
+    # num of rows (change to 3 for example, 8 for my input)
+    drawing_lines = 8
 
-    # NOTE: we are reading from the top down
+    parts = s.split("\n\n")
+    cargo = parts[0].split("\n")
+    procedures = parts[1].split("\n")
+    stacks = [[] for x in range(num_of_stacks)]
+
+    for i in range(drawing_lines):
+        line = cargo[i]
+        crates = line[1::4]
+        for s in range(len(crates)):
+            if crates[s] != " ":
+                stacks[s].append(crates[s])
+
+    # note: we are reading from the top down
     # but the crates have been put in from the bottom up
-    lines = s.splitlines()
-    for line in lines:
-        split_line = line.split(" ")
-        line_len: int = len(split_line)
+    # so, we need to reverse to represent real order
+    stacks = [stack[::-1] for stack in stacks]
 
-        # populate directions
-        if line.startswith("m"):
-            directions.append(int(split_line[1]))
-            directions.append(int(split_line[3]))
-            directions.append(int(split_line[5]))
-        # populate stacks with characters
-        else:
-            # 9 if one col populated, 6 if two, 3 if all three
-            if line_len == 9:
-                if split_line[0].startswith("["):
-                    stack_1.append(split_line[0][1])
-                elif split_line[4].startswith("["):
-                    stack_2.append(split_line[4][1])
-                elif split_line[8].startswith("["):
-                    stack_3.append(split_line[8][1])
-            elif line_len == 6:
-                if split_line[0].startswith("[") and split_line[5].startswith("["):
-                    stack_1.append(split_line[0][1])
-                    stack_3.append(split_line[5][1])
-                elif split_line[0].startswith("[") and split_line[1].startswith("["):
-                    stack_1.append(split_line[0][1])
-                    stack_2.append(split_line[1][1])
-                elif split_line[4].startswith("[") and split_line[5].startswith("["):
-                    stack_2.append(split_line[4][1])
-                    stack_3.append(split_line[5][1])
-            elif line_len == 3:
-                stack_1.append(split_line[0][1])
-                stack_2.append(split_line[1][1])
-                stack_3.append(split_line[2][1])
+    # move crates in the cargo
+    for procedure in procedures:
+        tokens = procedure.split(" ")
+        n, from_col, to_col = map(int, [tokens[1], tokens[3], tokens[5]])
+        # make things 0-indexed
+        from_col -= 1
+        to_col -= 1
+        # add/remove crates
+        for x in range(n):
+            crate = stacks[from_col].pop()
+            stacks[to_col].append(crate)
 
-    stack_1.reverse()
-    stack_2.reverse()
-    stack_3.reverse()
-
-    while len(directions) != 0:
-        amount = directions.popleft()
-        from_col = directions.popleft()
-        to_col = directions.popleft()
-        # move the crates
-        for i in range(0, amount):
-            if from_col == 1 and to_col == 2:
-                crate: chr = stack_1.pop()
-                stack_2.append(crate)
-            elif from_col == 1 and to_col == 3:
-                crate: chr = stack_1.pop()
-                stack_3.append(crate)
-            elif from_col == 1 and to_col == 1:
-                pass
-            elif from_col == 2 and to_col == 1:
-                crate: chr = stack_2.pop()
-                stack_1.append(crate)
-            elif from_col == 2 and to_col == 3:
-                crate: chr = stack_2.pop()
-                stack_3.append(crate)
-            elif from_col == 2 and to_col == 2:
-                pass
-            elif from_col == 3 and to_col == 1:
-                crate: chr = stack_3.pop()
-                stack_1.append(crate)
-            elif from_col == 3 and to_col == 2:
-                crate: chr = stack_3.pop()
-                stack_2.append(crate)
-            elif from_col == 3 and to_col == 3:
-                pass
-
-    return stack_1.pop() + stack_2.pop() + stack_3.pop()
+    return "".join([stack[-1] for stack in stacks])
 
 
 # TODO: change for the small example given
