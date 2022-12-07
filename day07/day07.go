40e849f00ed72cc3a91d6461da7d732afc95e051
maxdemaio <maxdman321@gmail.com>
2022-12-07 17:47:57 -0500

    finish day 7

--- day07/day07.go
+++ day07/day07.go
@@ -1,25 +1,149 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func Part1(data string) int {
-	for i := 0; i < len(data); i++ {
-		fmt.Println(data[i])
+func Part2(filepath string) int {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("can not open file")
 	}
 
-	return 0
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// stack of folders
+	fs := []string{}
+
+	// folder to folder filesize mappings
+	m := map[string]int{}
+
+	// for duplicate names
+	// we use counter to make the folder names unique
+	counter := 0
+
+	for scanner.Scan() {
+		// grab tokens
+		fields := strings.Fields(scanner.Text())
+		if fields[0] == "$" {
+			if fields[1] == "cd" {
+				if fields[2] != ".." {
+					dir := fields[2]
+					// if folder already found make unique w/ counter
+					if _, ok := m[fields[2]]; ok {
+						dir += strconv.Itoa(counter)
+						counter++
+					}
+					// add dir to folder stack
+					fs = append(fs, dir)
+
+				} else {
+					// go back one folder, pop last dir from stack
+					fs = fs[:len(fs)-1]
+				}
+			}
+		} else {
+			// we're on a file, add its file size to whole folder stack
+			if fields[0] != "dir" {
+				for i := 0; i < len(fs); i++ {
+					size, _ := strconv.Atoi(fields[0])
+					m[fs[i]] += size
+				}
+			}
+		}
+
+	}
+
+	// current units of unused disk
+	unused := 70000000 - m["/"]
+
+	// amount of units to remove
+	// to get us to 30mil free units
+	smallest := 30000000 - unused
+
+	// start min off as the biggest folder, "/"
+	min := m["/"]
+
+	for _, v := range m {
+		if v >= smallest && v < min {
+			min = v
+		}
+	}
+
+	return min
+}
+
+func Part1(filepath string) int {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("can not open file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// stack of folders
+	fs := []string{}
+
+	// folder to folder filesize mappings
+	m := map[string]int{}
+
+	// for duplicate names
+	// we use counter to make the folder names unique
+	counter := 0
+
+	for scanner.Scan() {
+		// grab tokens
+		fields := strings.Fields(scanner.Text())
+		if fields[0] == "$" {
+			if fields[1] == "cd" {
+				if fields[2] != ".." {
+					dir := fields[2]
+					// if folder already found make unique w/ counter
+					if _, ok := m[fields[2]]; ok {
+						dir += strconv.Itoa(counter)
+						counter++
+					}
+					// add dir to folder stack
+					fs = append(fs, dir)
+
+				} else {
+					// go back one folder, pop last dir from stack
+					fs = fs[:len(fs)-1]
+				}
+			}
+		} else {
+			// we're on a file, add its file size to whole folder stack
+			if fields[0] != "dir" {
+				for i := 0; i < len(fs); i++ {
+					size, _ := strconv.Atoi(fields[0])
+					m[fs[i]] += size
+				}
+			}
+		}
+
+	}
+
+	sum := 0
+	for _, v := range m {
+		if v <= 100000 {
+			sum += v
+		}
+	}
+
+	return sum
 }
 
 func main() {
-	data, err := os.ReadFile("./input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
 	// change byte[] to string
-	data_s := string(data)
-	fmt.Println("part1: ", Part1(data_s))
+	fmt.Println("part1: ", Part1("input.txt"))
+	fmt.Println("part2: ", Part2("input.txt"))
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -2,33 +2,17 @@ package main
 
 import "testing"
 
-var testStr string = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`
-
 func TestPart1(t *testing.T) {
-	result := Part1(testStr)
+	result := Part1("test.txt")
 	expected := 95437
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2("test.txt")
+	expected := 24933642
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
 	}
