44a28037611eed15424530ac2f0bef0888174825
maxdemaio <maxdman321@gmail.com>
2022-12-09 08:44:15 -0500

    add mv map

--- day09/day09.go
+++ day09/day09.go
@@ -11,13 +11,15 @@ import (
 	"github.com/maxdemaio/aoc-2022/utils"
 )
 
+var mvMap map[string][2]int
+
 func touching(x1 int, x2 int, y1 int, y2 int) bool {
 	xdif := utils.AbsDiffInt(x1, x2)
 	ydif := utils.AbsDiffInt(y1, y2)
 	return xdif <= 1 && ydif <= 1
 }
 
-func Part1(filepath string, n int) int {
+func Part1(filepath string, n int, mvMap map[string][2]int) int {
 	file, err := os.Open(filepath)
 	if err != nil {
 		log.Fatalf("can not open file")
@@ -44,6 +46,7 @@ func Part1(filepath string, n int) int {
 		fmt.Println()
 	}
 
+	fmt.Println(mvMap["R"])
 	return 0
 }
 
@@ -52,5 +55,12 @@ func Part2(filepath string) int {
 }
 
 func main() {
-	fmt.Printf("part2: %d", Part1("input.txt", 2))
+	// init move map of size 4 and add moves
+	mvMap = make(map[string][2]int, 4)
+	mvMap["R"] = [2]int{1, 0}
+	mvMap["U"] = [2]int{0, 1}
+	mvMap["L"] = [2]int{-1, 0}
+	mvMap["D"] = [2]int{0, -1}
+
+	fmt.Printf("part2: %d", Part1("input.txt", 2, mvMap))
 }
--- day09/day09_test.go
+++ day09/day09_test.go
@@ -1,11 +1,31 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestPart1(t *testing.T) {
-	result := Part1("test.txt", 2)
+	result := Part1("test.txt", 2, mvMap)
 	expected := 13
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
 	}
+}
+
+func TestMain(m *testing.M) {
+	// initialize the map in this function
+	mvMap = make(map[string][2]int, 4)
+	mvMap["R"] = [2]int{1, 0}
+	mvMap["U"] = [2]int{0, 1}
+	mvMap["L"] = [2]int{-1, 0}
+	mvMap["D"] = [2]int{0, -1}
+
+	// run the tests
+	code := m.Run()
+
+	// do any cleanup here, if necessary
+
+	// exit with the same code as the tests
+	os.Exit(code)
 }
